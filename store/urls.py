3244c73c048b874b91d12a3e26c68edfc8f0ff6c
botazitihssane <botazitihssane@outlook.com>
2023-05-05 16:18:05 +0100

    incompleted version

--- store/urls.py
+++ store/urls.py
@@ -3,16 +3,16 @@ from . import views
 from .controller import cart, checkout, authview
 
 urlpatterns = [
-    path('home', views.home, name="home"),
     path('add-to-cart', cart.addtocart, name="addtocart"),
     path('cart', cart.viewcart, name="cart"),
     path('update-cart', cart.updatecart, name="updatecart"),
     path('delete-cart-item', cart.deletecartitem, name="deletecartitem"),
     path('checkout', checkout.index, name="checkout"),
-    path('place-order',checkout.placeorder,name="placeorder"),
+    path('place-order', checkout.placeorder, name="placeorder"),
+    path('home', views.home, name="home"),
+    path('collections/', views.collections, name='collections'),
+    path('collections/<str:slug>', views.collectionsview, name='collectionsview'),
     path('register/', authview.register, name="register"),
     path('login/', authview.loginpage, name="loginpage"),
     path('logout/', authview.logout, name="logout"),
-    path('collections/', views.collections, name='collections'),
-    path('collections/<str:slug>', views.collectionsview, name='collectionsview')
 ]
--- store/views.py
+++ store/views.py
@@ -5,27 +5,25 @@ from store.models import Category, Product
 
 
 def home(request):
-    return render(request,"store/index.html")
-
-
-def home(request):
-    return render(request,"store/index.html")
+    return render(request, "store/index.html")
 
 def my_view(request):
     data = Product.objects.values('name')
     return render(request, 'store/inc/navbar.html', {'data': data})
 
+
 def collections(request):
     category = Category.objects.filter
-    context = {'category' : category}
-    return render(request,"store/collections.html",context)
+    context = {'category': category}
+    return render(request, "store/collections.html", context)
 
-def collectionsview(request,slug):
-    if(Category.objects.filter(slug=slug)):
+
+def collectionsview(request, slug):
+    if Category.objects.filter(slug=slug):
         products = Product.objects.filter(category__slug=slug)
-        category= Category.objects.filter(slug=slug).first()
-        context = {'products' :products, 'category':category}
-        return render(request,"store/products/index.html",context)
+        category = Category.objects.filter(slug=slug).first()
+        context = {'products': products, 'category': category}
+        return render(request, "store/products/index.html", context)
     else:
-        messages.warning(request,"no such category found")
+        messages.warning(request, "no such category found")
         return redirect('collections')
